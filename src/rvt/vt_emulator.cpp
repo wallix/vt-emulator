6b83b19b9d597c1b161798b093bf96360e955da6
jonathanpoelen <jonathan.poelen@gmail.com>
2017-03-08 15:43:28 +0100

    ignored hidden

--- src/rvt/vt_emulator.cpp
+++ src/rvt/vt_emulator.cpp
@@ -534,7 +534,7 @@ void VtEmulator::processToken(int token, int32_t p, int q)
     case TY_CSI_PS('m',   4) : _currentScreen->setRendition          (Rendition::Underline); break; //VT100
     case TY_CSI_PS('m',   5) : _currentScreen->setRendition          (Rendition::Blink    ); break; //VT100
     case TY_CSI_PS('m',   7) : _currentScreen->setRendition          (Rendition::Reverse  ); break;
-    case TY_CSI_PS('m',   8) : /* TODO _currentScreen->setRendition          (Rendition::Hidden   );*/ break;
+    case TY_CSI_PS('m',   8) : /* IGNORED: _currentScreen->setRendition          (Rendition::Hidden   );*/ break;
     case TY_CSI_PS('m',  10) : /* IGNORED: mapping related          */ break; //LINUX
     case TY_CSI_PS('m',  11) : /* IGNORED: mapping related          */ break; //LINUX
     case TY_CSI_PS('m',  12) : /* IGNORED: mapping related          */ break; //LINUX
@@ -544,7 +544,7 @@ void VtEmulator::processToken(int token, int32_t p, int q)
     case TY_CSI_PS('m',  24) : _currentScreen->resetRendition     (Rendition::Underline); break;
     case TY_CSI_PS('m',  25) : _currentScreen->resetRendition     (Rendition::Blink    ); break;
     case TY_CSI_PS('m',  27) : _currentScreen->resetRendition     (Rendition::Reverse  ); break;
-    case TY_CSI_PS('m',  28) : /* TODO _currentScreen->resetRendition     (Rendition::Hidden   ); */ break;
+    case TY_CSI_PS('m',  28) : /* IGNORED: _currentScreen->resetRendition     (Rendition::Hidden   ); */ break;
 
     case TY_CSI_PS('m',   30) : _currentScreen->setForeColor         (ColorSpace::System,  0); break;
     case TY_CSI_PS('m',   31) : _currentScreen->setForeColor         (ColorSpace::System,  1); break;
