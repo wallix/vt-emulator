2f6473a385107825fc143ef5d7b34085ca38d1b5
Jonathan Poelen <jonathan.poelen@gmail.com>
2020-06-12 10:20:25 +0200

    [test][jam] fix compilation with latest version of Boost.Test

--- redemption/src/system/linux/system/test_framework.cpp
+++ redemption/src/system/linux/system/test_framework.cpp
@@ -1,6 +1,2 @@
-#define BOOST_AUTO_TEST_MAIN
-#define BOOST_TEST_DYN_LINK
-
-#include <boost/test/included/unit_test.hpp>
 #include <boost/test/unit_test_monitor.hpp>
-#include "./redemption_unit_tests.hpp"
+#include <boost/test/included/unit_test.hpp>
