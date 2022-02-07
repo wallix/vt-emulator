5590ff81d7e59d887078c352424b18c2561a3be8
Jonathan Poelen <jonathan.poelen@gmail.com>
2022-02-07 13:06:24 +0100

    to_json: fast color to string number

--- src/rvt/text_rendering.cpp
+++ src/rvt/text_rendering.cpp
@@ -34,6 +34,26 @@ namespace rvt {
 namespace
 {
 
+enum class U8Color : uint8_t;
+
+static constexpr char digit10_pairs[200] = {
+    '0', '0', '0', '1', '0', '2', '0', '3', '0', '4', '0', '5', '0', '6',
+    '0', '7', '0', '8', '0', '9', '1', '0', '1', '1', '1', '2', '1', '3',
+    '1', '4', '1', '5', '1', '6', '1', '7', '1', '8', '1', '9', '2', '0',
+    '2', '1', '2', '2', '2', '3', '2', '4', '2', '5', '2', '6', '2', '7',
+    '2', '8', '2', '9', '3', '0', '3', '1', '3', '2', '3', '3', '3', '4',
+    '3', '5', '3', '6', '3', '7', '3', '8', '3', '9', '4', '0', '4', '1',
+    '4', '2', '4', '3', '4', '4', '4', '5', '4', '6', '4', '7', '4', '8',
+    '4', '9', '5', '0', '5', '1', '5', '2', '5', '3', '5', '4', '5', '5',
+    '5', '6', '5', '7', '5', '8', '5', '9', '6', '0', '6', '1', '6', '2',
+    '6', '3', '6', '4', '6', '5', '6', '6', '6', '7', '6', '8', '6', '9',
+    '7', '0', '7', '1', '7', '2', '7', '3', '7', '4', '7', '5', '7', '6',
+    '7', '7', '7', '8', '7', '9', '8', '0', '8', '1', '8', '2', '8', '3',
+    '8', '4', '8', '5', '8', '6', '8', '7', '8', '8', '8', '9', '9', '0',
+    '9', '1', '9', '2', '9', '3', '9', '4', '9', '5', '9', '6', '9', '7',
+    '9', '8', '9', '9',
+};
+
 struct RenderingBuffer2
 {
     RenderingBuffer2(RenderingBuffer buffer) noexcept
@@ -82,7 +102,6 @@ struct RenderingBuffer2
 
     void unsafe_push_ucs(ucs4_char ucs)
     {
-        // TODO optimize that
         switch (ucs) {
             case '\\': assert(remaining() >= 2); *_p++ = '\\'; *_p++ = '\\'; break;
             case '"': assert(remaining() >= 2); *_p++ = '\\'; *_p++ = '"'; break;
@@ -180,6 +199,25 @@ private:
     void _unsafe_push_value(chars_view x)
     {
         unsafe_push_s(x);
+    }
+
+    void _unsafe_push_value(U8Color x)
+    {
+        uint8_t value { underlying_cast(x) };
+        if (value < 100) {
+            if (value < 10) {
+                unsafe_push_c(char('0' + value));
+            }
+            else {
+                unsafe_push_c(digit10_pairs[value * 2]);
+                unsafe_push_c(digit10_pairs[value * 2 + 1]);
+            }
+        }
+        else {
+            unsafe_push_c(char('0' + value / 100));
+            unsafe_push_c(digit10_pairs[value % 100 * 2]);
+            unsafe_push_c(digit10_pairs[value % 100 * 2 + 1]);
+        }
     }
 
     // static std::size_t _value_size(int)
@@ -296,14 +334,18 @@ void json_rendering(
                         buf.unsafe_push_s("\"},{"_av);
                     }
                     if (!is_same_rendition) {
-                        // TODO table of u8
                         int const r = (0
                             | (bool(ch.rendition & rvt::Rendition::Bold)      ? 1 : 0)
                             | (bool(ch.rendition & rvt::Rendition::Italic)    ? 2 : 0)
                             | (bool(ch.rendition & rvt::Rendition::Underline) ? 4 : 0)
                             | (bool(ch.rendition & rvt::Rendition::Blink)     ? 8 : 0)
                         );
-                        buf.unsafe_push_values("\"r\":"_av, r, ',');
+                        if (r < 10) {
+                            buf.unsafe_push_values("\"r\":"_av, char(r + '0'), ',');
+                        }
+                        else {
+                            buf.unsafe_push_values("\"r\":"_av, '1', char(r - 10 + '0'), ',');
+                        }
                     }
 
                     if (!is_same_fg) {
@@ -361,11 +403,10 @@ void ansi_rendering(
 ) {
     auto write_color = [palette](RenderingBuffer2 & buf, char cmd, rvt::CharacterColor const & ch_color) {
         auto color = ch_color.color(palette);
-        // TODO table of color
         buf.unsafe_push_values(';', cmd, '8', ';', '2', ';',
-                               color.red()+0, ';',
-                               color.green()+0, ';',
-                               color.blue()+0);
+                               U8Color(color.red()), ';',
+                               U8Color(color.green()), ';',
+                               U8Color(color.blue()));
     };
 
     RenderingBuffer2 buf{buffer};
