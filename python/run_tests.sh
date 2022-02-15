ba41fc758182b35feff98e91c0aa48ccbacf29ad
Jonathan Poelen <jonathan.poelen@gmail.com>
2022-02-15 11:17:07 +0100

    format source code

--- python/run_tests.sh
+++ python/run_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 if [[ -n $1 ]]; then
   export LD_LIBRARY_PATH="$1"
 fi
--- python/wallix_term/wallix_term.py
+++ python/wallix_term/wallix_term.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 from ctypes import byref, cast, c_size_t, c_char, c_void_p, Array, addressof
 from enum import Enum
 from os import fsencode, strerror, PathLike
-from typing import Callable, Any, Optional, Union, Tuple
+from typing import Callable, Any, Optional, Union, Tuple, NamedTuple
 
 
 PathLikeObject = Union[str, bytes, PathLike]
@@ -24,20 +24,20 @@ TranscriptPrefix = lib.TerminalEmulatorTranscriptPrefix
 CreateFileMode = lib.TerminalEmulatorCreateFileMode
 
 
-Allocator = namedtuple('TerminalEmulatorAllocator',
-                        ['ctx',
-                         'get_buffer_fn',
-                         'extra_memory_allocator_fn',
-                         'set_final_buffer_fn',
-                         'clear_fn',
-                         'delete_ctx_fn'])
+class Allocator(NamedTuple):
+    ctx: Any
+    get_buffer_fn: Callable#[[void*,size_t*], char*]
+    extra_memory_allocator_fn: Callable#[[void*,size_t,char*,size_t], char*]
+    set_final_buffer_fn: Callable#[[void*,char*,size_t], None]
+    clear_fn: Callable#[[void*], None]
+    delete_ctx_fn: Callable#[[void*], None]
 
 
 class TerminalEmulatorException(Exception):
     pass
 
 
-def _check_errnum(errnum) -> None:
+def _check_errnum(errnum: int) -> None:
     if errnum == 0:
         return
 
@@ -53,7 +53,7 @@ def _check_errnum(errnum) -> None:
 class TerminalEmulator:
     __slot__ = ('_ctx')
 
-    def __init__(self, lines:int, columns:int, title:Optional[str]=None, allocator:Allocator=None) -> None:
+    def __init__(self, lines: int, columns: int, title: Optional[str] = None, allocator: Allocator = None) -> None:
         if allocator:
             self._ctx = lib.terminal_emulator_buffer_new_with_custom_allocator(
                 allocator.ctx,
@@ -74,20 +74,20 @@ class TerminalEmulator:
     def __del__(self) -> None:
         lib.terminal_emulator_delete(self._ctx)
 
-    def set_log_function(self, func:Callable[[str], None]) -> None:
+    def set_log_function(self, func: Callable[[str], None]) -> None:
         log_func = lambda p,n: func(p.decode())
         _check_errnum(lib.terminal_emulator_set_log_function(
             self._ctx, lib.TerminalEmulatorLogFunction(log_func)))
         # extend lifetime
         self._log_func = log_func
 
-    def set_title(self, title:str) -> None:
+    def set_title(self, title: str) -> None:
         _check_errnum(lib.terminal_emulator_set_title(self._ctx, title.encode()))
 
-    def feed(self, s:bytes) -> None:
+    def feed(self, s: bytes) -> None:
         _check_errnum(lib.terminal_emulator_feed(self._ctx, s, len(s)))
 
-    def resize(self, lines:int, columns:int) -> None:
+    def resize(self, lines: int, columns: int) -> None:
         _check_errnum(lib.terminal_emulator_resize(self._ctx, lines, columns))
 
 
@@ -102,7 +102,7 @@ class TerminalEmulatorBuffer:
     def __del__(self) -> None:
         lib.terminal_emulator_buffer_delete(self._ctx)
 
-    def prepare(self, emu:TerminalEmulator, format:OutputFormat, extra_data:Optional[bytes]=None) -> None:
+    def prepare(self, emu: TerminalEmulator, format: OutputFormat, extra_data: Optional[bytes] = None) -> None:
         if extra_data:
             _check_errnum(lib.terminal_emulator_buffer_prepare2(
                 self._ctx, emu._ctx, int(format), extra_data, len(extra_data)))
@@ -123,25 +123,25 @@ class TerminalEmulatorBuffer:
             raise TerminalEmulatorException('invalid buffer')
         return (addressof(p.contents), n.value)
 
-    def write(self, filename:PathLikeObject,
-              mode:int=0o664,
-              create_mode:CreateFileMode=CreateFileMode.fail_if_exists) -> None:
+    def write(self, filename: PathLikeObject,
+              mode: int = 0o664,
+              create_mode: CreateFileMode=CreateFileMode.fail_if_exists) -> None:
         _check_errnum(lib.terminal_emulator_buffer_write(self._ctx, fsencode(filename), mode, create_mode))
 
     def write_integrity(self,
-                        filename:PathLikeObject,
-                        mode:int=0o664,
-                        prefix_tmp_filename:Optional[PathLikeObject]=None) -> None:
+                        filename: PathLikeObject,
+                        mode: int = 0o664,
+                        prefix_tmp_filename: Optional[PathLikeObject] = None) -> None:
         filename = fsencode(filename)
         _check_errnum(lib.terminal_emulator_buffer_write_integrity(
             self._ctx, filename, fsencode(prefix_tmp_filename) if prefix_tmp_filename else filename, mode))
 
 
-def transcript_from_ttyrec(infile:PathLikeObject,
-                           outfile:Optional[PathLikeObject]=None,
-                           mode:int=0o664,
-                           create_mode:CreateFileMode=CreateFileMode.fail_if_exists,
-                           prefix_type:TranscriptPrefix=TranscriptPrefix.datetime) -> None:
+def transcript_from_ttyrec(infile: PathLikeObject,
+                           outfile: Optional[PathLikeObject] = None,
+                           mode: int = 0o664,
+                           create_mode: CreateFileMode = CreateFileMode.fail_if_exists,
+                           prefix_type: TranscriptPrefix = TranscriptPrefix.datetime) -> None:
     """
     Generate a transcript file of session recorded by ttyrec
     """
